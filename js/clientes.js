90f076d19b023579744422355371dd65394f6a11
Cássio Santos <sss.cassio@gmail.com>
2017-01-17 22:47:42 -0300

    Marcas autorizadas

--- js/clientes.js
+++ js/clientes.js
@@ -9,4 +9,9 @@ $(document).ready(function(){
         autoplay: true,
         controls:true,
     });
+    $('.slider-autorizadas').slick({
+        slidesToShow: 4, 
+        slidesToScroll: 4
+
+    });
 });
